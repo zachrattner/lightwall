44a985f268dbe869c7549752fe9101baa69194c4
Zach Rattner <zach@yembo.ai>
2025-09-14 09:26:11 -0700

    Set up addressing approach

--- firmware/boards/delta/delta.ino
+++ firmware/boards/delta/delta.ino
@@ -1,70 +1,78 @@
-// Baud: 115200, line endings: Newline (\n)
-
-/*
- * DELTA BOARD
- * Laser D6
- * Laser E0
- * Laser F6
- * Laser G1
- * Laser G3
- **/
-
 #include <Arduino.h>
 
-static const char name[8] = "DELTA";
-
+#define CMD_BUFFER_LENGTH 64
+#define NAME_MAX_LENGTH 8
 #define SERIAL_BAUD 115200
+#define COMMAND_READY_SEQUENCE "READY"
+
+static const char name[NAME_MAX_LENGTH] = "delta";
+static char cmd_buffer[CMD_BUFFER_LENGTH]= "";
+static uint8_t cmd_buffer_length; 
 
 static int32_t g_value = 12345;  // lives in RAM
 
-void setup() {
-  Serial.begin(SERIAL_BAUD);
-
-  // Give USB 2s to enumerate, but do not block forever
-  unsigned long start = millis();
-  while (!Serial && (millis() - start) < 2000) { }
-
-  Serial.println("READY");
+static inline bool is_newline(char c) {
+  return ((c == '\n') || (c == '\r'));
 }
 
-void loop() {
-  static char buf[64];
-  static size_t len = 0;
+static void serial_start() {
+  Serial.begin(SERIAL_BAUD);
+  Serial.println(COMMAND_READY_SEQUENCE);
+}
 
-  // Read bytes into a simple line buffer
+static void serial_process_cmd() {
+  if (strcasecmp(cmd_buffer, "NAME") == 0) {
+    Serial.println(name);
+  }
+  else {
+    Serial.println("ERR: Unknown cmd");
+  }
+}
+
+static void serial_read_char() {
   while (Serial.available() > 0) {
-    char c = (char)Serial.read();
+    char c = (char) Serial.read();
 
-    if (c == '\n' || c == '\r') {
+    if (is_newline(c)) {
       // End of line reached. Process command if we have any chars.
-      if (len > 0) {
-        buf[len] = '\0';
+      if (cmd_buffer_length > 0) {
+        cmd_buffer[cmd_buffer_length] = '\0';
 
-        // Trim trailing spaces
-        while (len > 0 && (buf[len - 1] == ' ' || buf[len - 1] == '\t')) {
-          buf[--len] = '\0';
+        // Null out trailing whitespace
+        while ( (cmd_buffer_length > 0) && 
+               ((cmd_buffer[cmd_buffer_length - 1] == ' ') || 
+                (cmd_buffer[cmd_buffer_length - 1] == '\t'))) {
+          cmd_buffer[--cmd_buffer_length] = '\0';
         }
 
-        // Process commands
-        if (strcasecmp(buf, "NAME") == 0) {
-          Serial.println(name);
-        }
-        else {
-          Serial.println("ERR: Unknown cmd");
-        }
+        serial_process_cmd();
       }
+
       // Reset buffer for next line
-      len = 0;
+      cmd_buffer_length = 0;
     }
     else {
-      if (len < (sizeof(buf) - 1)) {
-        buf[len++] = c;
+      if (cmd_buffer_length < (sizeof(cmd_buffer) - 1)) {
+        cmd_buffer[cmd_buffer_length++] = c;
       }
       else {
         // Overflow: reset and report
-        len = 0;
+        cmd_buffer_length = 0;
         Serial.println("ERR: Overflow");
       }
     }
   }
+}
+
+void setup() {
+  memset(cmd_buffer, 0, CMD_BUFFER_LENGTH);
+  cmd_buffer_length = 0;
+
+  serial_start();
+}
+
+void loop() {
+  serial_read_char(); 
+
+  // Do other useful work here
 }
